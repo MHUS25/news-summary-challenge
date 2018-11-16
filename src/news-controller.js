d4ad9cecd3792fa5979928ea596b159d3402a331
Muna <mmhussein125@gmail.com>
2018-11-16 21:47:28 +0000

    Linking each headline to a page that displays the full article

--- src/news-controller.js
+++ src/news-controller.js
@@ -9,5 +9,32 @@
     document.getElementById('app').innerHTML = html;
   };
 
+  NewsController.prototype.makeUrlChangeShowNewsForCurrentPage = function() {
+    var boundShowNewsForCurrentPage = this._showNewsForCurrentPage.bind(this)
+    window.addEventListener("hashchange", boundShowNewsForCurrentPage);
+  };
+
+  NewsController.prototype._showNewsForCurrentPage = function() {
+    var newsId = this._getNewsIdFromUrl(window.location);
+    var news = this._findNewsById(newsId)
+    this._showNews(news);
+  };
+
+  NewsController.prototype._getNewsIdFromUrl = function(location) {
+    return location.hash.split("#news/")[1];
+  };
+
+  NewsController.prototype._showNews = function(news) {
+    var singleNewsView = new SingleNewsView(news)
+    var div = document.getElementById('app');
+    div.innerHTML = singleNewsView.viewNews();
+  };
+
+  NewsController.prototype._findNewsById = function(id) {
+    return this.newsList.getArticles().find(function(news) {
+      return String(news.id) === id;
+    })
+  };
+
   exports.NewsController = NewsController
 })(this)
