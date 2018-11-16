e49aa2fd192d4cd459def83c633ecdcb13f7923d
Muna <mmhussein125@gmail.com>
2018-11-16 20:31:07 +0000

    News-list view

---
+++ spec/news-list-view-tests.js
@@ -0,0 +1,14 @@
+function testNewsListViewStartsEmpty() {
+  var newsList = new NewsList
+  var newsListView = new NewsListView(newsList)
+  assert.isEqualTo(newsListView.newsListHTML(), '<ul></ul>')
+}
+testNewsListViewStartsEmpty()
+ function testNewsListViewWithNotesAdded() {
+  var newsList = new NewsList
+  var newsListView = new NewsListView(newsList)
+  newsList.addArticle('Brexit', 'May facing pressure over Brexit');
+  newsList.addArticle('Earthquake Today', 'Solomon Islands, Indonesia and Japan suffers HUGE quakes as 6.2 tremors');
+  assert.isEqualTo(newsListView.newsListHTML(), '<ul><li><div><a href="#news/0">Brexit</a></div></li><li><div><a href="#news/1">Earthquake Today</a></div></li></ul>')
+}
+testNewsListViewWithNotesAdded()
---
+++ src/news-list-view.js
@@ -0,0 +1,13 @@
+
+(function(exports) {
+  function NewsListView(newsList) {
+    this.newsList = newsList
+  }
+   NewsListView.prototype.newsListHTML = function() {
+    list = this.newsList.getArticles().map(
+      news => `<li><div><a href="#news/${news.getId()}">${news.getHeadline()}</a></div></li>`
+    ).join('')
+    return `<ul>${list}</ul>`
+  }
+   exports.NewsListView = NewsListView
+})(this)
