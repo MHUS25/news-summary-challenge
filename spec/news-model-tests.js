a8fb7be75fc1ac0c4021c1bded118daaba1f6106
Muna <mmhussein125@gmail.com>
2018-11-16 20:27:28 +0000

    News model

---
+++ spec/news-model-tests.js
@@ -0,0 +1,15 @@
+function testNewsInstantiatedWithHeadline() {
+  var news = new News('News Headline', 'News body', 0);
+  assert.isEqualTo(news.getHeadline(), 'News Headline');
+};
+ testNewsInstantiatedWithHeadline();
+ function testNewsInstantiatedWithBody() {
+  var news = new News('News Headline', 'News body', 0);
+  assert.isTrue(news.getBody() === 'News body');
+};
+ testNewsInstantiatedWithBody();
+ function testNewsInstantiatedWithId() {
+  var news = new News('News Headline', 'News body', 0);
+  assert.isTrue(news.getId() === 0);
+};
+ testNewsInstantiatedWithId();
---
+++ src/news-model.js
@@ -0,0 +1,17 @@
+(function(exports) {
+  function News(headline, body, id) {
+    this.headline = headline;
+    this.body = body
+    this.id = id;
+  };
+   News.prototype.getHeadline = function() {
+    return this.headline
+  };
+   News.prototype.getBody = function() {
+    return this.body
+  };
+   News.prototype.getId = function() {
+    return this.id
+  };
+   exports.News = News;
+})(this);
