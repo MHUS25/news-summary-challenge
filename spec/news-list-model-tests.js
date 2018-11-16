ae656c00deff73a10e7f4ebf0796476148ba17f3
Muna <mmhussein125@gmail.com>
2018-11-16 20:43:28 +0000

    Formats files

--- spec/news-list-model-tests.js
+++ spec/news-list-model-tests.js
@@ -2,6 +2,7 @@ function testNewsListStartsWithNoNewsArticles() {
   var newsList = new NewsList();
   assert.isTrue(newsList.getArticles().length === 0);
 };
+
 testNewsListStartsWithNoNewsArticles();
 
 function testCanAddAndStoreNewsArticles() {
@@ -10,6 +11,7 @@ function testCanAddAndStoreNewsArticles() {
   assert.isEqualTo(newsList.getArticles()[0].getHeadline(), 'Brexit');
   assert.isEqualTo(newsList.getArticles()[0].getBody(), 'May facing pressure over Brexit');
 };
+
 testCanAddAndStoreNewsArticles();
 
 function testCreatesEachNewsArticleWithUniqueId() {
@@ -19,4 +21,5 @@ function testCreatesEachNewsArticleWithUniqueId() {
   assert.isEqualTo(newsList.getArticles()[0].getId(), 0);
   assert.isEqualTo(newsList.getArticles()[1].getId(), 1);
 };
+
 testCreatesEachNewsArticleWithUniqueId()
--- spec/news-model-tests.js
+++ spec/news-model-tests.js
@@ -2,16 +2,19 @@ function testNewsInstantiatedWithHeadline() {
   var news = new News('News Headline', 'News body', 0);
   assert.isEqualTo(news.getHeadline(), 'News Headline');
 };
+
 testNewsInstantiatedWithHeadline();
 
 function testNewsInstantiatedWithBody() {
   var news = new News('News Headline', 'News body', 0);
   assert.isTrue(news.getBody() === 'News body');
 };
+
 testNewsInstantiatedWithBody();
 
 function testNewsInstantiatedWithId() {
   var news = new News('News Headline', 'News body', 0);
   assert.isTrue(news.getId() === 0);
 };
+
 testNewsInstantiatedWithId();
